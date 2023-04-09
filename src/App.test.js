f8a4929066c8da09ae4bb07c73ad15115e3a876a
georg-nikola <gn.pavlov96@gmail.com>
2023-04-09 19:39:35 +0000

    Regenerate package-lock and fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Expense Tracker title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Expense Tracker/i);
+  expect(titleElement).toBeInTheDocument();
 });
